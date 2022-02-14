c944d0ddcafde3411349c04f22973b566b812682
이재근 <ljg960730@etoos.com>
2022-02-14 18:40:57 +0900

    initial commit

---
+++ src/test/java/io/cresendo/oauth2/SpringOauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cresendo.oauth2;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
